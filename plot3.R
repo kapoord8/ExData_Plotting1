3a88fe4f0694c3014ee9494bbee076af8e23a524
kapoord8 <71174449+kapoord8@users.noreply.github.com>
2021-06-01 14:41:23 +0530

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -8,14 +8,12 @@ hpc_data$Time <- strptime(hpc_data$Time, format = "%H:%M:%S")
 hpc_data[1:1440, "Time"] <- format(hpc_data[1:1440, "Time"], "2007-02-01 %H:%M:%S")
 hpc_data[1441:2880, "Time"] <- format(hpc_data[1441:2880, "Time"], "2007-02-02 %H:%M:%S")
 
-plot(hpc_data$Time, hpc_data$Sub_metering_1, xlab="", ylab= "Energy Sub Metering", type="n", 
-     main="Energy sub-metering")
+plot(hpc_data$Time, hpc_data$Sub_metering_1, xlab="", ylab= "Energy Sub Metering", type="n")
 axis(2, seq(0,30,10))
-par()
 
 lines(hpc_data$Time,as.numeric(as.character(hpc_data$Sub_metering_1)))
 lines(hpc_data$Time,as.numeric(as.character(hpc_data$Sub_metering_2)), col="red")
 lines(hpc_data$Time,as.numeric(as.character(hpc_data$Sub_metering_3)), col="blue")
 
 legend("topright", lty=1, col=c("black","red","blue"), 
-       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.8)
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
